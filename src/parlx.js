be9c241443d9c4f9897f82248aa79a03ee559aa2
Jakub Biesiada <jakubbiesiada@iMac-Jakub.local>
2018-04-01 09:22:00 +0200

    Update for beta 4

--- src/parlx.js
+++ src/parlx.js
@@ -1,5 +1,5 @@
 /*!
-* parlx.js v1.1 beta 3
+* parlx.js v1.1 beta 4
 * Copyright 2017-2018 Jakub Biesiada
 * MIT License
 */
@@ -32,8 +32,8 @@ class Parlx {
 
   init(elements, settings) {
     // split parallax elements
-    for (var element = 0; element < elements.length; element++) {
-      new Parlx(elements[element], settings);
+    for (let i = 0; i < elements.length; i++) {
+      this.parlx = new Parlx(elements[i], settings);
     }
   }
 
@@ -45,24 +45,27 @@ class Parlx {
     window.addEventListener('resize', () => this.onWindowResize());
   }
 
-  isTouch() {
-    // check if is touch device
-    if ("ontouchstart" in document.documentElement) {
-      return true;
-    }
-  }
-
   onWindowScroll() {
-    if (!this.isTouch()) {
-      // optimize parallax effect on desktop (no touch)
-      window.requestAnimationFrame(() => this.parallaxEffect());
-    } else {
-      this.parallaxEffect();
-    }
+    this.parallaxEffect();
   }
 
   onWindowResize() {
     this.parallaxEffect();
+  }
+
+  transforms() {
+    if (this.settings.direction === 'horizontal') {
+      this.moveX = this.movement;
+      this.moveY = '0';
+    } else if (this.settings.direction === 'vertical') {
+      this.moveX = '0';
+      this.moveY = this.movement;
+    } else if (this.settings.direction === 'diagonal') {
+      this.moveX = this.movement;
+      this.moveY = this.movement;
+    }
+
+    this.transform = `translate(${this.moveX}px, ${this.moveY}px)`;
   }
 
   parallaxEffect() {
@@ -70,77 +73,39 @@ class Parlx {
     this.element.style.height = this.settings.height;
 
     // get parallax scroll position
-    let scrolled = window.pageYOffset - this.element.offsetTop;
+    let scrolled = this.element.getBoundingClientRect().y;
 
     // set speed range
-    if (Math.abs(this.settings.speed) > 0.5) {
-      this.settings.speed = 0.3;
-    }
+    if (Math.abs(this.settings.speed) > 1) this.settings.speed = 0.3;
 
     // element movement
-    this.movement = (-this.settings.speed * scrolled).toFixed(2);
+    this.movement = (this.settings.speed * scrolled) / 2;
 
     // disable parallax on mobile if option mobile is false
-    if (this.isTouch() && !this.settings.mobile) {
-      this.settings.speed = 0;
-    }
+    if ('ontouchstart' in document.documentElement && !this.settings.mobile) this.settings.speed = 0;
+
+    // set transforms
+    this.transforms();
 
     // types of parallax direction
-    if (this.settings.type === 'front') {
-      if (this.settings.direction === 'horizontal') {
-        this.moveX = this.movement;
-        this.moveY = '0';
-      } else if (this.settings.direction === 'vertical') {
-        this.moveX = '0';
-        this.moveY = this.movement;
-      } else if (this.settings.direction === 'diagonal') {
-        this.moveX = this.movement;
-        this.moveY = this.movement;
-      }
-
-      if (this.isTouch()) {
-        this.transform = `translate(${this.moveX}px, ${this.moveY}px)`;
-      } else {
-        this.transform = `translate3d(${this.moveX}px, ${this.moveY}px, 0px)`;
-      }
+    if (this.settings.type === 'foreground') {
 
       // children element style
       Object.assign(this.element.style, {
         '-webkit-transform': this.transform,
+        'transform': this.transform
+      });
+    } else if (this.settings.type === 'background') {
+
+      // set image position
+      Object.assign(this.element.querySelector('img').style, {
+        '-webkit-transform': this.transform,
         'transform': this.transform,
-        'transition': 'transform linear',
-        'will-change': 'transform'
+        'width': 'auto',
+        'height': 'auto',
+        'min-width': `${this.element.offsetWidth * (1 + Math.abs(this.settings.speed))}px`,
+        'min-height': `${this.element.offsetHeight * (1 + Math.abs(this.settings.speed))}px`
       });
-    } else if (this.settings.type === 'back') {
-      Object.assign(this.element.style, {
-        'background-repeat': 'no-repeat'
-      });
-
-      // if width > height
-      if (this.element.offsetWidth > this.element.offsetHeight) {
-        Object.assign(this.element.style, {
-          'background-size': `${this.element.offsetWidth} ${this.element.offsetHeight * (1 + Math.abs(this.settings.speed) * 2)}px`
-        });
-
-      // if width <= height
-      } else {
-        Object.assign(this.element.style, {
-          'background-size': `${this.element.offsetWidth * (1 + Math.abs(this.settings.speed) * 2)}px ${this.element.offsetHeight}`
-        });
-      }
-
-      if (this.settings.direction === 'horizontal') {
-        this.posX = `calc(50% + ${this.movement}px)`;
-        this.posY = 'center';
-      } else if (this.settings.direction === 'vertical') {
-        this.posX = 'center';
-        this.posY = `calc(50% + ${this.movement}px)`;
-      } else if (this.settings.direction === 'diagonal') {
-        this.posX = `calc(50% + ${this.movement}px)`;
-        this.posY = `calc(50% + ${this.movement}px)`;
-      }
-
-      this.element.style.backgroundPosition = `${this.posX} ${this.posY}`;
     }
   }
 
@@ -148,16 +113,16 @@ class Parlx {
     // defaults
     let defaults = {
       direction: 'vertical', // parallax element move direction
-      type: 'back', // type of parallax: front (div move), back (background or inner image move)
+      type: 'background', // type of parallax: foreground (div move), background (inner image move)
       speed: 0.3, // parallax speed (min: -0.5, max: 0.5)
       height: '400px', // parallax element height
       mobile: true // enable: true, or disable: false, parallax on mobile devices (touch screen)
-    }
+    };
 
     let custom = {};
 
     // apply settings and get values from data-*
-    for (var setting in defaults) {
+    for (let setting in defaults) {
       if (setting in settings) {
         custom[setting] = settings[setting];
       } else if (this.element.getAttribute(`data-${setting}`)) {
@@ -176,24 +141,25 @@ class Parlx {
   }
 }
 
+// autoinit
+let autoInit = new Parlx(document.querySelectorAll('[data-parlx]'));
+
 // jQuery
 if (window.jQuery) {
   let $ = window.jQuery;
 
-  $.fn.Parlx = function(options) {
-    for (var element = 0; element < this.length; element++) {
-      new Parlx(this[element], options);
-    }
+  $.fn.parlx = function(options) {
+    new Parlx(this, options);
   }
 }
 
 // AMD
-if (typeof define === "function" && define.amd) {
-  define("Parlx", [], function() {
+if (typeof define === 'function' && define.amd) {
+  define('Parlx', [], function() {
     return Parlx;
   });
 
-// Common JS
+// CommonJS
 } else if (typeof exports !== 'undefined' && !exports.nodeType) {
   if (typeof module !== 'undefined' && !module.nodeType && module.exports) {
     exports = module.exports = Parlx;
