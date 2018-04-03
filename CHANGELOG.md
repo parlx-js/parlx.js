b402aaed9d2063ccdbe0c23a979205610d87120d
Jakub Biesiada <biesiadajakub@icloud.com>
2018-04-03 20:23:47 +0200

    Added .gitattributes

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+
+## 1.1.0 beta 4
+* **redesigned demo page (new header view, dark theme)**
+* **re-added support for image tag as a background**
+* **added autoinit**
+* fixed parallax background size bug
+* updated README.md
+* **renamed jQuery plugin from Parlx() to parlx()**
+* code cleaned up
+* **renamed type option values (`back` to `background` and `front` to `foreground`)**
+
+## 1.1.0 beta 3
+* npm added
+
+## 1.1.0 beta 2
+* AMD & CommonJS support added
+
+## 1.1.0 beta 1
+* **new parallax directions (`horizontal` and `diagonal`)**
+* **new option (type: `front` / `back`)**
+* updated demo page (system fonts, new images, new parallax elements and options)
+
+## 1.0.1
+* Single parallax element error fixed
--- dist/parlx.babel.js
+++ dist/parlx.babel.js
@@ -5,7 +5,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /*!
-* parlx.js v1.1 beta 3
+* parlx.js v1.1 beta 4
 * Copyright 2017-2018 Jakub Biesiada
 * MIT License
 */
@@ -44,8 +44,8 @@ var Parlx = function () {
     key: 'init',
     value: function init(elements, settings) {
       // split parallax elements
-      for (var element = 0; element < elements.length; element++) {
-        new Parlx(elements[element], settings);
+      for (var i = 0; i < elements.length; i++) {
+        this.parlx = new Parlx(elements[i], settings);
       }
     }
   }, {
@@ -64,31 +64,30 @@ var Parlx = function () {
       });
     }
   }, {
-    key: 'isTouch',
-    value: function isTouch() {
-      // check if is touch device
-      if ("ontouchstart" in document.documentElement) {
-        return true;
-      }
-    }
-  }, {
     key: 'onWindowScroll',
     value: function onWindowScroll() {
-      var _this2 = this;
-
-      if (!this.isTouch()) {
-        // optimize parallax effect on desktop (no touch)
-        window.requestAnimationFrame(function () {
-          return _this2.parallaxEffect();
-        });
-      } else {
-        this.parallaxEffect();
-      }
+      this.parallaxEffect();
     }
   }, {
     key: 'onWindowResize',
     value: function onWindowResize() {
       this.parallaxEffect();
+    }
+  }, {
+    key: 'transforms',
+    value: function transforms() {
+      if (this.settings.direction === 'horizontal') {
+        this.moveX = this.movement;
+        this.moveY = '0';
+      } else if (this.settings.direction === 'vertical') {
+        this.moveX = '0';
+        this.moveY = this.movement;
+      } else if (this.settings.direction === 'diagonal') {
+        this.moveX = this.movement;
+        this.moveY = this.movement;
+      }
+
+      this.transform = 'translate(' + this.moveX + 'px, ' + this.moveY + 'px)';
     }
   }, {
     key: 'parallaxEffect',
@@ -97,77 +96,39 @@ var Parlx = function () {
       this.element.style.height = this.settings.height;
 
       // get parallax scroll position
-      var scrolled = window.pageYOffset - this.element.offsetTop;
+      var scrolled = this.element.getBoundingClientRect().y;
 
       // set speed range
-      if (Math.abs(this.settings.speed) > 0.5) {
-        this.settings.speed = 0.3;
-      }
+      if (Math.abs(this.settings.speed) > 1) this.settings.speed = 0.3;
 
       // element movement
-      this.movement = (-this.settings.speed * scrolled).toFixed(2);
+      this.movement = this.settings.speed * scrolled / 2;
 
       // disable parallax on mobile if option mobile is false
-      if (this.isTouch() && !this.settings.mobile) {
-        this.settings.speed = 0;
-      }
+      if ('ontouchstart' in document.documentElement && !this.settings.mobile) this.settings.speed = 0;
+
+      // set transforms
+      this.transforms();
 
       // types of parallax direction
-      if (this.settings.type === 'front') {
-        if (this.settings.direction === 'horizontal') {
-          this.moveX = this.movement;
-          this.moveY = '0';
-        } else if (this.settings.direction === 'vertical') {
-          this.moveX = '0';
-          this.moveY = this.movement;
-        } else if (this.settings.direction === 'diagonal') {
-          this.moveX = this.movement;
-          this.moveY = this.movement;
-        }
-
-        if (this.isTouch()) {
-          this.transform = 'translate(' + this.moveX + 'px, ' + this.moveY + 'px)';
-        } else {
-          this.transform = 'translate3d(' + this.moveX + 'px, ' + this.moveY + 'px, 0px)';
-        }
+      if (this.settings.type === 'foreground') {
 
         // children element style
         Object.assign(this.element.style, {
           '-webkit-transform': this.transform,
+          'transform': this.transform
+        });
+      } else if (this.settings.type === 'background') {
+
+        // set image position
+        Object.assign(this.element.querySelector('img').style, {
+          '-webkit-transform': this.transform,
           'transform': this.transform,
-          'transition': 'transform linear',
-          'will-change': 'transform'
+          'width': 'auto',
+          'height': 'auto',
+          'min-width': this.element.offsetWidth * (1 + Math.abs(this.settings.speed)) + 'px',
+          'min-height': this.element.offsetHeight * (1 + Math.abs(this.settings.speed)) + 'px'
         });
-      } else if (this.settings.type === 'back') {
-        Object.assign(this.element.style, {
-          'background-repeat': 'no-repeat'
-        });
-
-        // if width > height
-        if (this.element.offsetWidth > this.element.offsetHeight) {
-          Object.assign(this.element.style, {
-            'background-size': this.element.offsetWidth + ' ' + this.element.offsetHeight * (1 + Math.abs(this.settings.speed) * 2) + 'px'
-          });
-
-          // if width <= height
-        } else {
-          Object.assign(this.element.style, {
-            'background-size': this.element.offsetWidth * (1 + Math.abs(this.settings.speed) * 2) + 'px ' + this.element.offsetHeight
-          });
-        }
-
-        if (this.settings.direction === 'horizontal') {
-          this.posX = 'calc(50% + ' + this.movement + 'px)';
-          this.posY = 'center';
-        } else if (this.settings.direction === 'vertical') {
-          this.posX = 'center';
-          this.posY = 'calc(50% + ' + this.movement + 'px)';
-        } else if (this.settings.direction === 'diagonal') {
-          this.posX = 'calc(50% + ' + this.movement + 'px)';
-          this.posY = 'calc(50% + ' + this.movement + 'px)';
-        }
-
-        this.element.style.backgroundPosition = this.posX + ' ' + this.posY;
       }
     }
   }, {
@@ -176,7 +137,7 @@ var Parlx = function () {
       // defaults
       var defaults = {
         direction: 'vertical', // parallax element move direction
-        type: 'back', // type of parallax: front (div move), back (background or inner image move)
+        type: 'background', // type of parallax: foreground (div move), background (inner image move)
         speed: 0.3, // parallax speed (min: -0.5, max: 0.5)
         height: '400px', // parallax element height
         mobile: true // enable: true, or disable: false, parallax on mobile devices (touch screen)
@@ -207,26 +168,27 @@ var Parlx = function () {
   return Parlx;
 }();
 
+// autoinit
+
+
+var autoInit = new Parlx(document.querySelectorAll('[data-parlx]'));
+
 // jQuery
-
-
 if (window.jQuery) {
   var $ = window.jQuery;
 
-  $.fn.Parlx = function (options) {
-    for (var element = 0; element < this.length; element++) {
-      new Parlx(this[element], options);
-    }
+  $.fn.parlx = function (options) {
+    new Parlx(this, options);
   };
 }
 
 // AMD
-if (typeof define === "function" && define.amd) {
-  define("Parlx", [], function () {
+if (typeof define === 'function' && define.amd) {
+  define('Parlx', [], function () {
     return Parlx;
   });
 
-  // Common JS
+  // CommonJS
 } else if (typeof exports !== 'undefined' && !exports.nodeType) {
   if (typeof module !== 'undefined' && !module.nodeType && module.exports) {
     exports = module.exports = Parlx;
