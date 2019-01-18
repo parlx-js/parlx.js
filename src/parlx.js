50888fd1c7c7bb8b9515a78cb541786b8f37c252
Jakub Biesiada <biesiadajakub@icloud.com>
2019-01-18 01:11:42 +0100

    Update

--- src/parlx.js
+++ src/parlx.js
@@ -79,6 +79,13 @@ export default class Parlx {
         transform: this.transform
       });
     } else if (this.settings.type === 'background') {
+      if (!this.element.querySelector('.parlx-children')) {
+        const child = document.createElement('div');
+        child.classList.add('parlx-children');
+
+        this.element.appendChild(child);
+      }
+
       Object.assign(this.element.querySelector('.parlx-children').style, {
         transform: this.transform,
         'object-fit': 'cover',
