9cbd5962c3c92a9aa89c79679451d8c0cf0959a6
Jakub Biesiada <biesiadajakub@icloud.com>
2019-04-21 15:48:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8ef21f87ae98801ca26c47f5fbc4cda1.js"
+  "./precache-manifest.256593914b01cfc797290291640978f4.js"
 );
 
 workbox.clientsClaim();
